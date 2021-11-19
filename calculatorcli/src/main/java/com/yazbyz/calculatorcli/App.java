6e8ab31f19a10ae6ce22f22f9c5dd89cead1f08a
Hyejin Eom <yazbyz@gmail.com>
2021-11-19 23:47:12 +0900

    [UPDATE] Spring Test - JUnit

---
+++ calculatorcli/src/main/java/com/yazbyz/calculatorcli/App.java
@@ -0,0 +1,13 @@
+package com.yazbyz.calculatorcli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
